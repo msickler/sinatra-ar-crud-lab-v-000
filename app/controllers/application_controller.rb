3cda854d3c11fe7780c913b7a21151b449313e3c
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-07 23:15:09 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,29 +8,40 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/posts/new' do
-    erb :new
-  end
-
-  get '/posts' do
-    @posts = Post.all
-    erb :index
-  end
-
-  get '/posts/:id' do
-    @post = Post.find(params[:id])
-    erb :show
-  end
-
-  get '/posts/:id/edit' do
+     erb :new
+   end
+ 
+   post '/posts' do
+     @post = Post.create(params)
+     erb :index
+   end
+ 
+   get '/posts' do
+     @posts = Post.all
+     erb :index
+   end
+ 
+   get '/posts/:id' do
+     @post = Post.find(params[:id])
+     erb :show
+   end
+ 
+   get '/posts/:id/edit' do
      @post = Post.find(params[:id])
      erb :edit
-  end
-
-  delete '/posts/:id/delete' do
-    @post = Post.find(params[:id])
-    @post.delete
-    erb :delete
-  end
-
-
-end
+   end
+ 
+   patch '/posts/:id' do
+     @post = Post.find(params[:id])
+     @post.name = params[:name]
+     @post.content = params[:content]
+     @post.save
+     erb :show
+   end
+ 
+   delete '/posts/:id/delete' do
+     @post = Post.find(params[:id])
+     @post.delete
+     erb :delete
+   end
+ end
