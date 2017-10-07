5a32086edef26fc6bbb981c41d58dd96400030df
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-07 23:15:15 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,27 +10,27 @@ class ApplicationController < Sinatra::Base
   get '/posts/new' do
      erb :new
    end
- 
+
    post '/posts' do
      @post = Post.create(params)
      erb :index
    end
- 
+
    get '/posts' do
      @posts = Post.all
      erb :index
    end
- 
+
    get '/posts/:id' do
      @post = Post.find(params[:id])
      erb :show
    end
- 
+
    get '/posts/:id/edit' do
      @post = Post.find(params[:id])
      erb :edit
    end
- 
+
    patch '/posts/:id' do
      @post = Post.find(params[:id])
      @post.name = params[:name]
@@ -38,7 +38,7 @@ class ApplicationController < Sinatra::Base
      @post.save
      erb :show
    end
- 
+
    delete '/posts/:id/delete' do
      @post = Post.find(params[:id])
      @post.delete
